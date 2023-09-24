eaec5fe01ccbcd52a912837108f01d16492481d4
Shiffayafathima <144373765+Shiffayafathima@users.noreply.github.com>
2023-09-24 11:42:41 +0530

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,16 @@
+
+def linearsearchproduct(productlist,targetproduct):
+  indices=[]
+
+  for index,product in enumerate(productlist):
+    if product==targetproduct:
+      indices.append(index)
+
+  return indices
+
+products=["shirts","t-shirs","pants","shirts","shoes","slipper","shirts"]
+
+target="shirts"
+result=linearsearchproduct(products,target)
+print(result)  
+  
